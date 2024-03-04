45ed7a45d5d60d93ced700726d3a3e5b2ee99595
Ricardo Melo <ricardomelo.rt@gmail.com>
2024-03-04 00:06:06 -0300

    update

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
 document.getElementById('imcForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const peso = parseFloat(document.getElementById('peso').value);
-    const altura = parseFloat(document.getElementById('altura').value);
+    const peso = Number(parseFloat(document.getElementById('peso').value));
+    const altura = Number(document.getElementById('altura').value.replace(',', '.'));
+
+    // const peso = Number(parseFloat(document.getElementById('peso').value));
+    // const altura = Number(parseFloat(document.getElementById('altura').value)).replace(',','.');
 
     const imc = peso / (altura * altura);
 
+    
     let situation;
-
+    
     if (imc < 16) {
-        situation = 'muito abaixo do peso (Magreza grave)';
+        situation = 'muito abaixo do peso (Magreza grave)'
+        const sitExtraLower = document.getElementById('extra-lower')
+        sitExtraLower.style.backgroundColor='rgb(185, 0, 0)'
+
     } else if (imc < 17) {
-        situation = 'muito abaixo do peso (Magreza moderada)';
+        situation = 'muito abaixo do peso (Magreza moderada)'
+        const sitLower = document.getElementById('lower')
+        sitLower.style.backgroundColor='rgb(235, 0, 0)'
+
     } else if (imc < 18.5) {
-        situation = 'abaixo do peso';
+        situation = 'abaixo do peso'
+        const sitLow = document.getElementById('extra-lower')
+        sitLow.style.backgroundColor='rgb(230, 230, 0)'
+
     } else if (imc < 25) {
-        situation = 'com peso normal';
+        situation = 'com peso normal'
+        const sitNormal = document.getElementById('normal')
+        sitNormal.style.backgroundColor='rgb(0, 100, 0)'
+
     } else if (imc < 30) {
-        situation = 'com sobrepeso';
+        situation = 'com sobrepeso'
+        const sitPlus = document.getElementById('plus')
+        sitPlus.style.backgroundColor='rgb(230, 230, 0)'
+
     } else if (imc < 35) {
-        situation = 'com obesidade leve';
+        situation = 'com obesidade leve'
+        const sitPlus2 = document.getElementById('plus-2')
+        sitPlus2.style.backgroundColor
+
     } else if (imc < 40) {
-        situation = 'com obesidade moderada';
+        situation = 'com obesidade moderada'
+        const sitPlus3 = document.getElementById('plus-3')
+        sitPlus3.style.backgroundColor='rgb(185, 0, 0)'
+
     } else {
-        situation = 'com obesidade grave';
+        situation = 'com obesidade grave'
+        const sitPlus4 = document.getElementById('plus-4')
+        sitPlus4.style.backgroundColor='rgb(185, 0, 0)'
+
     }
 
     const resultadoElement = document.getElementById('result');
@@ -35,25 +63,3 @@ document.getElementById('imcForm').addEventListener('submit', function(event) {
 
 
 document.getElementById('result').style.display = 'none'
-
-/*
-document.getElementById('imcForm').onsubmit = function(event) {
-    event.preventDefault();
-
-    const peso = +document.getElementById('peso').value;
-    const altura = +document.getElementById('altura').value;
-
-    const imc = peso / (altura * altura);
-    document.getElementById('resultado').textContent = `O seu IMC é: ${imc.toFixed(2)}`;
-};
-*/
-
-/*
-document.getElementById('calcularIMC').addEventListener('click', function() {
-    const peso = +document.getElementById('peso').value;
-    const altura = +document.getElementById('altura').value;
-
-    const imc = peso / (altura * altura);
-    document.getElementById('resultado').textContent = `O seu IMC é: ${imc.toFixed(2)}`;
-});
-*/
